d6c137891bfe3099fd175dfecebc15c94b0b38e3
NamNK <namnk@bkav.com>
2018-06-26 16:30:24 +0700

    Modify ValueUnit.

--- src/main/java/com/bkav/command/data/time/ValueUnit.java
+++ src/main/java/com/bkav/command/data/time/ValueUnit.java
@@ -2,19 +2,29 @@ package com.bkav.command.data.time;
 
 public class ValueUnit {
 	
+	// Common UNIT
 	public static final String CELSIUS_UNIT_CHAR = "\u2103";
 	public static final String PERCENT_UNIT_CHAR = "%";
+	public static final String EMPTY_UNIT_CHAR = "";
+	
+	//AIR UNIT
+	public static final String AIR_SPEED = "air_fan";
+	public static final String AIR_SWING = "air_swing";
 	
 	public ValueUnit(int value, String unit) {
 		this.value = value;
 		this.unit = unit;
 	}
 	
-	public int getValue() {
+	public ValueUnit(int value) {
+		this(value, EMPTY_UNIT_CHAR);
+	}
+	
+	public int value() {
 		return this.value;
 	}
 	
-	public String getUnit() {
+	public String unit() {
 		return this.unit;
 	}
 	
